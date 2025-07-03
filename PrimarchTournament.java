69139ac4d6d0c58710d380c0c51ebcaf1003281d
Вик <_email@example.com>
2025-07-04 01:20:25 +0300

    Добавил симулятор турнира Примархов

---
+++ PrimarchTournament.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
+public class PrimarchTournament {
+    public static void main(String[] args) {
+        // Зададим примархов (будет чесно добавить всех)
+        String[] allPrimaechs = {"Хорус Луперкаль", "Леман Русс", "Феррус Манус", "Фулгрим", "Вулкан", "Рогал Дорн",
+                "Робаут Жиллиман", "Магнус Красный", "Сангвиний", "Лев Эль'Джонсон", "Пертурабо", "Мортарион",
+                "Лоргар Аврелиан", "Джагатай Хан", "Конрад Кёрз", "Ангрон", "Корвус Коракс", "Альфарий Омегон"};
+
+        // Зададим им рандомный рейтинг
+        Random random = new Random();
+        // Увеличил стартовое значение рейтинга, чтобы была выше вероятность попадания в турнир
+        int[] allRating = {random.nextInt(50, 100), random.nextInt(50, 100),
+                random.nextInt(50, 100), random.nextInt(50,100),
+                random.nextInt(50, 100), random.nextInt(50,100),
+                random.nextInt(50,100), random.nextInt(50,100),
+                random.nextInt(50,100), random.nextInt(50,100),
+                random.nextInt(50,100), random.nextInt(50,100),
+                random.nextInt(50,100), random.nextInt(50,100),
+                random.nextInt(50,100), random.nextInt(50,100),
+                random.nextInt(50,100), random.nextInt(50,100)};
+
+        //Собираем коллекцию прошедших турнир
+        ArrayList<String> primarchs = new ArrayList<>();
+        ArrayList<Integer> ratings = new ArrayList<>();
+
+        // Посмотрим на примархов, их рейтинги и отбираем для турнира
+        for (int i = 0; i < allPrimaechs.length; i++) {
+            System.out.println("Примарх: " + allPrimaechs[i] + ", с рейтингом: " + allRating[i]);
+            // В турнир попадают только с рейтингов выше 80
+            if (allRating[i] > 80) {
+                System.out.println(allPrimaechs[i] + " допущен к турниру");
+                primarchs.add(allPrimaechs[i]);
+                ratings.add(allRating[i]);
+            } else {
+                System.out.println(allPrimaechs[i] + " не прошел отбор");
+            }
+        }
+
+        // Проводим турнир
+        if (primarchs.size() < 3) {
+            System.out.println("Недостаточно участников");
+
+        } else {
+            for (int i = 0; i < primarchs.size(); i++) {
+                int currentRating = ratings.get(i);
+                for (int j = 0; j < 3; j++) {
+                    currentRating += random.nextInt(1, 10);
+                }
+                ratings.set(i, currentRating);
+                System.out.println("Примарх: " + primarchs.get(i) + " по результатам 3-х раундов набрал " +
+                        ratings.get(i) + " очков");
+
+            }
+        }
+
+        // Результаты турнира
+        if (primarchs.size() >= 3) {
+            Integer max = Collections.max(ratings);
+
+            // Находим индекс максимального элемента
+            int maxIndex = ratings.indexOf(max);
+
+            System.out.println("Победитель турнира, примарх: " + primarchs.get(maxIndex) +
+                    " набравший больше всех очков в рейтинге: " + ratings.get(maxIndex));
+        }
+
+    }
+}
